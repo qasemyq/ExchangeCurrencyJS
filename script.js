1ef84b471e893f5086f9f6ce45814f761217456b
Ghader Ghasemi <68222268+qasemyq@users.noreply.github.com>
2021-05-21 22:43:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const input = document.getElementById('myInput')
-// event listner input fire mishe vagti value dakhle tage input, select va textarea change beshe  
 input.addEventListener('input',function(event){
     let converted = event.target.value
     let dollarValue = document.getElementById('dollarId')
-    // 
     dollarValue.innerHTML = converted * 365
     let poundValue = document.getElementById('poundId')
     poundValue.innerHTML = converted * 420
